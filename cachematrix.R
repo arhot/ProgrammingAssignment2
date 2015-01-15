57e02c45137a6f0e13da255a9568bdbd333c516d
unknown <atoikka@BL-WPTP-1601.ads.iu.edu>
2015-01-15 16:55:42 -0500

    Completed course assignment.
    Think it works.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is programming assignment 2 for R programming
+## There are two functions, makeCacheMatrix and cacheSolve
+## cacheSolve checks if there is an inverted matrix in the cache, if not then solve and cache
+## makeCacheMatrix provides the tools for the operation and storing values in appropriate environments
 
-## Write a short comment describing this function
+
+## makeCacheMatrix makes a matrix into a list of four functions 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+## set the matrix to y
+  setMatrix <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+## return the matrix
+  getMatrix <- function() x
+## set the cache solution to inverted
+  setInver <- function(inverted) m <<- inverted
+## return the cached solution
+getInver <- function() m
+## makeCacheMatrix returns the list of the four functions
+  list(setMatrix = setMatrix, getMatrix = getMatrix,
+       setInver = setInver,
+       getInver = getInver)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve checks if there is a cached solution (), if not then calculate and caches one, returns inverted matrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+## Is there an inverted solution, if there is, print a message and the solution 
+  m <- x$getInver()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+## if there was no solution, get the original, invert it, cache and return the solution
+  data <- x$getMatrix()
+  m <- solve(data)
+  x$setInver(m)
+  m
 }
